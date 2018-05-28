0eece8e0e8b85d774838de6e8789b099cec8c7eb
ralbin <russell.albin@blueacorn.com>
2018-05-28 06:12:15 -0500

    Adding website group and store view logic

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Blueacorn\registration.php
- * @version     1.0.5
+ * @version     1.0.6
  * @author      Blue Acorn <code@blueacorn.com>
  * @copyright   Copyright © 2018 Blue Acorn.
  */
